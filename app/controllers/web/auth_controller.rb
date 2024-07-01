4cd38204c14b63239605cbdc3c42a9cdcc7e64d2
Alexander <Alexandr_kitchenko@mail.ru>
2024-07-01 17:34:02 +0600

    add auth logging

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -10,6 +10,8 @@ module Web
         sign_in(user)
         flash[:notice] = t('auth.signed_in')
       else
+        logger.debug "==== USER NOT SAVED: #{user.errors}"
+
         flash[:alert] = t('auth.error')
       end
 
@@ -27,8 +29,15 @@ module Web
       user_email = user_data['email'].downcase
       user_name = user_data['name']
 
+      logger.debug "==== USER_EMAIL: #{user_email}"
+      logger.debug "==== USER_NAME: #{user_name}"
+
       user = User.find_or_initialize_by(email: user_email)
       user.name = user_name
+
+      logger.debug "==== USER ATTRS: #{user.attributes.inspect}"
+      logger.debug "==== USER VALID?: #{user.valid?}"
+
       user
     end
   end

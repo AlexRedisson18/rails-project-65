74bb88e4208398583e2897eb6cf49a58b0690f8c
Alexander <Alexandr_kitchenko@mail.ru>
2024-07-01 18:06:22 +0600

    remove stdout

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -10,8 +10,6 @@ module Web
         sign_in(user)
         flash[:notice] = t('auth.signed_in')
       else
-        puts "==== USER NOT SAVED: #{user.errors.full_messages}"
-
         flash[:alert] = t('auth.error')
       end
 
@@ -29,15 +27,8 @@ module Web
       user_email = user_data['email'].downcase
       user_name = user_data['name']
 
-      puts "==== USER_EMAIL: #{user_email}"
-      puts "==== USER_NAME: #{user_name}"
-
       user = User.find_or_initialize_by(email: user_email)
       user.name = user_name
-
-      puts "==== USER ATTRS: #{user.attributes.inspect}"
-      puts "==== USER VALID?: #{user.valid?}"
-
       user
     end
   end
